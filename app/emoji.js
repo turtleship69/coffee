a9b7784d7fb6f3c24a8bf0da48277219c0777c83
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2025-01-21 23:18:53 +0000

    rework emoji picker and remove extra dependencies

--- app/emoji.js
+++ app/emoji.js
@@ -1,13 +1,14 @@
-import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
 import insertText from 'https://cdn.jsdelivr.net/npm/insert-text-at-cursor@0.3.0/index.js'
 
-const button = document.querySelector('button')
 const tooltip = document.querySelector('#emoji-picker')
-Popper.createPopper(button, tooltip)
 
 document.querySelector('#emoji').onclick = () => {
     tooltip.classList.toggle('shown');
-    document.removeEventListener("keydown", event)
+    // document.removeEventListener("keydown", event)
+}
+
+document.querySelector("#emoji-background").onclick = () => {
+    tooltip.classList.remove("shown");
 }
 
 // document.addEventListener("keydown", (event) => {
