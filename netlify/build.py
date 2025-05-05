f97011779ee67394055f2f736243474833ff0717
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2025-05-05 11:58:05 +0100

    fix: format code and update redirect rules in build script for consistency

--- netlify/build.py
+++ netlify/build.py
@@ -1,27 +1,29 @@
 import os
 
-hanko_url = os.environ.get('HANKO_URL', 'https://e3c4803f-48cc-4b62-9ac5-2aa02444ba51.hanko.io')
-with open('login/hanko.js', 'w') as f:
+hanko_url = os.environ.get(
+    "HANKO_URL", "https://e3c4803f-48cc-4b62-9ac5-2aa02444ba51.hanko.io"
+)
+with open("login/hanko.js", "w") as f:
     f.write(f'var HANKO_URL = "{hanko_url}";\n')
 
 
-live_url = os.environ.get('SOCKET_URL', 'window.location.host')
-with open('app/live_server_url.js', 'w') as f:
-    f.write(f'let live_server_url = {live_url};')
+live_url = os.environ.get("SOCKET_URL", "window.location.host")
+with open("app/live_server_url.js", "w") as f:
+    f.write(f"let live_server_url = {live_url};")
 
 
-#define url rewrite rules
-redirects = ["auth", "connect", "update_profile", "chatlist.json", "chats", "new_chat"]
+# define url rewrite rules
+redirects = ["auth", "chatlist.json", "connect", "new_chat", "update_profile", "chats/"]
 netlify = ""
 
 for redirect in redirects:
     netlify += f"""
 [[redirects]]
-from = "/{redirect}/*"
-to = "{os.environ.get("BACKEND_URL")}/{redirect}/:splat"
+from = "/{redirect}*"
+to = "{os.environ.get("BACKEND_URL")}/{redirect}:splat"
 status = 200
 force = true
 """
 
-with open("netlify.toml", 'w') as f:
+with open("netlify.toml", "w") as f:
     f.write(netlify)
